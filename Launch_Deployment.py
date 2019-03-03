59e7a9f4e7efa09f539487ca451ad4c748e02574
Christian Biasi <christianbiasi@gmx.de>
2019-03-03 20:26:35 +0100

    added gyrX as gloabl variable, reduced berryIMU thread to runKalman method

--- Launch_Deployment.py
+++ Launch_Deployment.py
@@ -1,9 +1,11 @@
 # This class starts the Server.py as Server and berryIMU.py as berryIMU as separate threads.
 # This way it's possible to both calculate on the Raspberry Pi and provide the kalmanX and kalmanY values to a client.
 # It includes globalKalman.py as globalKalman to provide kalmanX and kalmanY as shared variables for both threads
+from _thread import start_new_thread
 
 import berryIMU
 import globalKalman
+import threading
 from Server import initServer
 
 globalKalman.initGlobalKalmanX()
@@ -12,9 +14,9 @@ print("globalKalman.X")
 globalKalman.initglobalKalmanY()
 # debugging
 print("globalKalman.Y")
+globalKalman.initGyrXangle()
 
-berry = berryIMU.CalculationClass()
-berry.start()
+readValues = threading.Thread(target = berryIMU.runKalmanThread())
 # debugging
 print("berry start")
 initServer()
