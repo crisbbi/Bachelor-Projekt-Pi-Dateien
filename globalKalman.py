e1e3e6e5c70f77b7a486ace94aee0f59a74ae9e9
Christian Biasi <christianbiasi@gmx.de>
2019-03-03 18:38:27 +0100

    provide global kalman viariables for shared use

---
+++ globalKalman.py
@@ -0,0 +1,9 @@
+# This file provides global variables for shared use
+
+def initGlobalKalmanX():
+    global kalmanX
+    kalmanX = 0.0
+
+def initglobalKalmanY():
+    global kalmanY
+    kalmanY = 0.0
