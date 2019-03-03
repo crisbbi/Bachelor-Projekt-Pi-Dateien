e822e87e643af6b449c19d116be33236063e33a6
Christian Biasi <christianbiasi@gmx.de>
2019-03-03 18:45:34 +0100

    built new Server with flask to provide kalmanX

--- Server.py
+++ Server.py
@@ -1,38 +1,13 @@
-import web
-import math
-from threading import Thread
-import calibrateBerryIMU
-import time
-import IMU
+# This file generates a Server using flask.
+# It utilizes globalKalman.py as globalKalman to provide access to the shared global variable kalmanX and kalmanY
 
+import flask
+import globalKalman
 
-urls = (
-    '/', 'Index'
-)
+def initServer():
+    app = flask.Flask(__name__)
+    @app.route('/')
+    def index():
+        return str(globalKalman.kalmanX)
 
-def get_y_rotation(x, y, z):
-    radians = math.atan2(x, dist(y, z))
-    return -math.degrees(radians)
-
-
-def get_x_rotation(x, y, z):
-    radians = math.atan2(y, dist(x, z))
-    return math.degrees(radians)
-
-
-def dist(a, b):
-    return math.sqrt((a*a)+(b*b))
-
-class Index:
-    def GET(self):
-        gyr_x = IMU.readACCx()
-        gyr_y = IMU.readACCy()
-        gyr_z = IMU.readACCz()
-
-        return str(get_x_rotation(gyr_x,gyr_y,gyr_z)) + " " + str(get_y_rotation(gyr_x,gyr_y,gyr_z))
-        return " " + str(IMU.readGYRx()) + " " + str(IMU.readGYRy()) + " " + str(IMU.readGYRz())
-        return " " + str(berryIMU.gyroXangle) + " " + str(berryIMU.gyroYangle) + " " + str(berryIMU.gyroZangle)
-
-if __name__ == "__main__":
-    app = web.application(urls, globals())
-    app.run()
+    app.run(debug = True, host = '0.0.0.0')
